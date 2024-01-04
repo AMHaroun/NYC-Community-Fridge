fc24df70c2097ce98f210ab18b7e37113a5b2665
Smig <ahmedharoun370@gmail.com>
2024-01-04 09:25:46 -0500

    Added FridgeLogo() Composable to LoginScreen(), Started work on Title() Composable

---
+++ app/src/main/java/com/example/nyccommunityfridge/ui/loginscreen/Title.kt
@@ -0,0 +1,21 @@
+package com.example.nyccommunityfridge.ui.loginscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nyccommunityfridge.ui.theme.NYCCommunityFridgeTheme
+
+@Composable
+fun Title(
+    modifier: Modifier = Modifier
+){
+
+}
+
+@Preview
+@Composable
+fun TitlePreview(){
+    NYCCommunityFridgeTheme {
+        Title()
+    }
+}
