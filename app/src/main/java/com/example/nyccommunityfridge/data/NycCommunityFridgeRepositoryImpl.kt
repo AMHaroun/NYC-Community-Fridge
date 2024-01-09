f53ef896d33a14cbec7bc11d0f18d3ebca47c683
Smig <ahmedharoun370@gmail.com>
2024-01-09 13:21:31 -0500

    Created NycCommunityFridgeRepository

---
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package com.example.nyccommunityfridge.data
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class NycCommunityFridgeRepositoryImpl(
+    val remoteDatabase : FirebaseFirestore
+): NycCommunityFridgeRepository {
+
+}
