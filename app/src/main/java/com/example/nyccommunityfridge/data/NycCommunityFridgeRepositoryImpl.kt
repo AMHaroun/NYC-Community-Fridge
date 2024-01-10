ca3c004f446c71ffe00d5d0b516d5abb0c5772e2
Smig <ahmedharoun370@gmail.com>
2024-01-10 10:28:15 -0500

    Setup local database provider methods, & now providing localDatabaseDao to NycCommunityFridgeRepositoryImpl

--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.example.nyccommunityfridge.data
 
+import com.example.nyccommunityfridge.data.local.room.dao.CommunityFridgeDao
 import com.google.firebase.firestore.FirebaseFirestore
 
 class NycCommunityFridgeRepositoryImpl(
-    val remoteDatabase : FirebaseFirestore
+    val remoteDatabase : FirebaseFirestore,
+    val localDatabaseDao: CommunityFridgeDao
 ): NycCommunityFridgeRepository {
 
 }
--- app/src/main/java/com/example/nyccommunityfridge/di/AppModule.kt
+++ app/src/main/java/com/example/nyccommunityfridge/di/AppModule.kt
@@ -1,19 +1,40 @@
 package com.example.nyccommunityfridge.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.nyccommunityfridge.data.NycCommunityFridgeRepository
 import com.example.nyccommunityfridge.data.NycCommunityFridgeRepositoryImpl
+import com.example.nyccommunityfridge.data.local.room.dao.CommunityFridgeDao
+import com.example.nyccommunityfridge.data.local.room.database.CommunityFridgeDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideLocalDatabase(@ApplicationContext context: Context): CommunityFridgeDatabase{
+        return Room.databaseBuilder(
+            context,
+            CommunityFridgeDatabase::class.java,
+            "CommunityFridgeDB"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLocalDatabaseDao(localDatabase: CommunityFridgeDatabase): CommunityFridgeDao {
+        return localDatabase.communityFridgeDao
+    }
 
     @Singleton
     @Provides
@@ -24,11 +45,13 @@ object AppModule {
     @Singleton
     @Provides
     fun provideNycCommunityFridgeRepository(
-        remoteDatabase: FirebaseFirestore
+        remoteDatabase: FirebaseFirestore,
+        localDatabaseDao: CommunityFridgeDao
     ): NycCommunityFridgeRepository {
 
         return NycCommunityFridgeRepositoryImpl(
-            remoteDatabase = remoteDatabase
+            remoteDatabase = remoteDatabase,
+            localDatabaseDao = localDatabaseDao
         )
 
     }
