768d64fcab3109044cd72986f872c1126dbf5102
Smig <ahmedharoun370@gmail.com>
2024-01-11 22:25:33 -0500

    Defined methods for NycCommunityFridgeRepository

--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
@@ -1,6 +1,16 @@
 package com.example.nyccommunityfridge.data
 
+import com.example.nyccommunityfridge.model.CommunityFridge
+
 interface NycCommunityFridgeRepository {
+
+    suspend fun getRemoteCommunityFridges(): List<CommunityFridge>
+
+    suspend fun getLocalCommunityFridges(): List<CommunityFridge>
+
+    suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>)
+
+    suspend fun checkForCommunityFridgeUpdates(): Boolean
 
 
 }
--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
@@ -1,11 +1,27 @@
 package com.example.nyccommunityfridge.data
 
 import com.example.nyccommunityfridge.data.local.room.dao.CommunityFridgeDao
+import com.example.nyccommunityfridge.model.CommunityFridge
 import com.google.firebase.firestore.FirebaseFirestore
 
 class NycCommunityFridgeRepositoryImpl(
     val remoteDatabase : FirebaseFirestore,
     val localDatabaseDao: CommunityFridgeDao
 ): NycCommunityFridgeRepository {
+    override suspend fun getRemoteCommunityFridges(): List<CommunityFridge> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getLocalCommunityFridges(): List<CommunityFridge> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun checkForCommunityFridgeUpdates(): Boolean {
+        TODO("Not yet implemented")
+    }
 
 }
