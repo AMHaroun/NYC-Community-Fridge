7a50fd6d5901fd9233ef657f4b6989c82b1b09d2
Smig <ahmedharoun370@gmail.com>
2024-01-12 21:54:02 -0500

    Implemented getLocalCOmmunityFridges() in NycCommunityFridgeRepositoryImpl

--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
@@ -1,12 +1,13 @@
 package com.example.nyccommunityfridge.data
 
 import com.example.nyccommunityfridge.model.CommunityFridge
+import kotlinx.coroutines.flow.Flow
 
 interface NycCommunityFridgeRepository {
 
     suspend fun getRemoteCommunityFridges(): List<CommunityFridge>
 
-    suspend fun getLocalCommunityFridges(): List<CommunityFridge>
+    suspend fun getLocalCommunityFridges(): Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>>
 
     suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>)
 
--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.nyccommunityfridge.data
 
+import android.util.Log
 import com.example.nyccommunityfridge.data.local.room.dao.CommunityFridgeDao
 import com.example.nyccommunityfridge.model.CommunityFridge
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 class NycCommunityFridgeRepositoryImpl(
     val remoteDatabase : FirebaseFirestore,
@@ -12,8 +15,15 @@ class NycCommunityFridgeRepositoryImpl(
         TODO("Not yet implemented")
     }
 
-    override suspend fun getLocalCommunityFridges(): List<CommunityFridge> {
-        TODO("Not yet implemented")
+    override suspend fun getLocalCommunityFridges(): Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>> {
+        val result = try {
+            localDatabaseDao.getCommunityFridges()
+        } catch (e: Exception){
+            e.printStackTrace()
+            Log.d("DEBUG", "getLocalCommunityFridges() {${e.message}}")
+            return emptyFlow()
+        }
+        return result
     }
 
     override suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>) {
