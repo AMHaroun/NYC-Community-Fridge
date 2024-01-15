4749df7797cbdb4620ba339af4dc28859e9f98ce
Smig <ahmedharoun370@gmail.com>
2024-01-15 15:46:23 -0500

    Updated CommunityFridge Repository to use the Resource class

--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepository.kt
@@ -1,13 +1,14 @@
 package com.example.nyccommunityfridge.data
 
 import com.example.nyccommunityfridge.model.CommunityFridge
+import com.example.nyccommunityfridge.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface NycCommunityFridgeRepository {
 
-    suspend fun getRemoteCommunityFridges(): List<CommunityFridge>
+    suspend fun getRemoteCommunityFridges(): Resource<List<CommunityFridge>>
 
-    suspend fun getLocalCommunityFridges(): Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>>
+    suspend fun getLocalCommunityFridges(): Resource<Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>>>
 
     suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>)
 
--- app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
+++ app/src/main/java/com/example/nyccommunityfridge/data/NycCommunityFridgeRepositoryImpl.kt
@@ -3,27 +3,27 @@ package com.example.nyccommunityfridge.data
 import android.util.Log
 import com.example.nyccommunityfridge.data.local.room.dao.CommunityFridgeDao
 import com.example.nyccommunityfridge.model.CommunityFridge
+import com.example.nyccommunityfridge.util.Resource
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 
 class NycCommunityFridgeRepositoryImpl(
     val remoteDatabase : FirebaseFirestore,
     val localDatabaseDao: CommunityFridgeDao
 ): NycCommunityFridgeRepository {
-    override suspend fun getRemoteCommunityFridges(): List<CommunityFridge> {
+    override suspend fun getRemoteCommunityFridges(): Resource<List<CommunityFridge>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getLocalCommunityFridges(): Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>> {
+    override suspend fun getLocalCommunityFridges(): Resource<Flow<List<com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge>>> {
         val result = try {
             localDatabaseDao.getCommunityFridges()
         } catch (e: Exception){
             e.printStackTrace()
             Log.d("DEBUG", "getLocalCommunityFridges() {${e.message}}")
-            return emptyFlow()
+            return Resource.Error(message = "Could not get fridge data from offline database")
         }
-        return result
+        return Resource.Success(result)
     }
 
     override suspend fun saveCommunityFridges(communityFridges: List<CommunityFridge>) {
