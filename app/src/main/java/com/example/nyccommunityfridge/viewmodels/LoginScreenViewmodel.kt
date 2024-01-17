94687341c5a7b539954a79c7735b89d8e8e232c1
Smig <ahmedharoun370@gmail.com>
2024-01-16 20:46:04 -0500

    Setup viewmodel package & classes for FridgeMapScreen & LoginScreen

---
+++ app/src/main/java/com/example/nyccommunityfridge/viewmodels/LoginScreenViewmodel.kt
@@ -0,0 +1,8 @@
+package com.example.nyccommunityfridge.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class LoginScreenViewmodel : ViewModel() {
+}
