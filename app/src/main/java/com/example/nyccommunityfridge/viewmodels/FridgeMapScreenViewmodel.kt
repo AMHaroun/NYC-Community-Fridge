c9acfa7233e039c4faf151a8627abcdf5df9fe68
Smig <ahmedharoun370@gmail.com>
2024-01-17 21:53:33 -0500

    Created uiState class for FridgeMapScreenViewmodel

--- app/src/main/java/com/example/nyccommunityfridge/viewmodels/FridgeMapScreenViewmodel.kt
+++ app/src/main/java/com/example/nyccommunityfridge/viewmodels/FridgeMapScreenViewmodel.kt
@@ -1,7 +1,14 @@
 package com.example.nyccommunityfridge.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge
 import dagger.hilt.android.lifecycle.HiltViewModel
+
+sealed interface FridgeMapScreenUiState{
+    data class Success(val fridges: List<CommunityFridge>)
+
+    data class Error(val message: String) : FridgeMapScreenUiState
+}
 
 @HiltViewModel
 class FridgeMapScreenViewmodel : ViewModel() {
