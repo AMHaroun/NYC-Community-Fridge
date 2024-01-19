95bb56ab397c6d2a5cb5b0e9a3699834cae35449
Smig <ahmedharoun370@gmail.com>
2024-01-18 21:38:49 -0500

    Created uiState for LoginScreenViewmodel

--- app/src/main/java/com/example/nyccommunityfridge/viewmodels/LoginScreenViewmodel.kt
+++ app/src/main/java/com/example/nyccommunityfridge/viewmodels/LoginScreenViewmodel.kt
@@ -3,6 +3,8 @@ package com.example.nyccommunityfridge.viewmodels
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
+data class LoginScreenUiState(var loggedIn: Boolean)
+
 @HiltViewModel
 class LoginScreenViewmodel : ViewModel() {
 }
