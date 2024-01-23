24dc747cdc9f56e42f8e7c914ab4fd4226fd3003
Smig <ahmedharoun370@gmail.com>
2024-01-23 09:38:55 -0500

    Implemented getRemoteCommunityFridgeData() in FridgeMapScreenViewmodel

--- app/src/main/java/com/example/nyccommunityfridge/viewmodels/FridgeMapScreenViewmodel.kt
+++ app/src/main/java/com/example/nyccommunityfridge/viewmodels/FridgeMapScreenViewmodel.kt
@@ -1,20 +1,39 @@
 package com.example.nyccommunityfridge.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.nyccommunityfridge.data.local.room.entity.CommunityFridge
+import androidx.lifecycle.viewModelScope
+import com.example.nyccommunityfridge.data.NycCommunityFridgeRepository
+import com.example.nyccommunityfridge.model.CommunityFridge
+import com.example.nyccommunityfridge.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 sealed interface FridgeMapScreenUiState{
-    data class Success(val fridges: List<CommunityFridge>)
+    data class Success(val fridges: List<CommunityFridge>): FridgeMapScreenUiState
 
     data class Error(val message: String) : FridgeMapScreenUiState
 }
 
 @HiltViewModel
-class FridgeMapScreenViewmodel : ViewModel() {
+class FridgeMapScreenViewmodel @Inject constructor(val repository: NycCommunityFridgeRepository) : ViewModel() {
 
+    var uiState : FridgeMapScreenUiState by mutableStateOf(FridgeMapScreenUiState.Success(listOf()))
+    private set
     fun getRemoteCommunityFridgeData(){
-        //TODO : Get Community Fridge data from firestore
+        viewModelScope.launch {
+            val result = repository.getRemoteCommunityFridges()
+
+            when(result){
+                is Resource.Error -> { uiState = FridgeMapScreenUiState.Error(result.message)}
+                is Resource.Loading -> TODO()
+                is Resource.Success -> { uiState = FridgeMapScreenUiState.Success(result.data)}
+            }
+        }
+
     }
 
     fun getLocalCommunityFridgeData(){
--- app/src/main/java/com/example/nyccommunityfridge/viewmodels/LoginScreenViewmodel.kt
+++ app/src/main/java/com/example/nyccommunityfridge/viewmodels/LoginScreenViewmodel.kt
@@ -2,11 +2,12 @@ package com.example.nyccommunityfridge.viewmodels
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 data class LoginScreenUiState(var loggedIn: Boolean)
 
 @HiltViewModel
-class LoginScreenViewmodel : ViewModel() {
+class LoginScreenViewmodel @Inject constructor() : ViewModel() {
 
     fun isUserLoggedIn(){
         //TODO
