186bb86cc96311673c1fcf76ee95609375bd68c2
Smig <ahmedharoun370@gmail.com>
2024-01-26 16:28:12 -0500

    Setup Navigation in NYCComuityFridgeApp() with NavHost composable

--- app/src/main/java/com/example/nyccommunityfridge/ui/NYCCommunityFridgeApp.kt
+++ app/src/main/java/com/example/nyccommunityfridge/ui/NYCCommunityFridgeApp.kt
@@ -2,10 +2,32 @@ package com.example.nyccommunityfridge.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.nyccommunityfridge.ui.fridgemapscreen.FridgeMapScreen
+import com.example.nyccommunityfridge.ui.loginscreen.LoginScreen
+import com.example.nyccommunityfridge.util.NavigationStrings
 
 @Composable
 fun NYCCommunityFridgeApp(
     modifier: Modifier = Modifier
 ){
-   // TODO: Handle navigation between screens here
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = NavigationStrings.fridgeMapScreen,
+        modifier = modifier
+    ){
+        composable(NavigationStrings.loginScreen){
+            LoginScreen(
+                loginWithGoogle = { /*TODO*/ },
+                loginWithFacebook = { /*TODO*/ },
+                skipLogin = { /*TODO*/ }
+            )
+        }
+        composable(NavigationStrings.fridgeMapScreen){
+            FridgeMapScreen()
+        }
+    }
 }
---
+++ app/src/main/java/com/example/nyccommunityfridge/util/NavigationStrings.kt
@@ -0,0 +1,6 @@
+package com.example.nyccommunityfridge.util
+
+object NavigationStrings {
+    const val loginScreen = "Login_Screen"
+    const val fridgeMapScreen = "Fridge_Map_Screen"
+}
