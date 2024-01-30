5f7eddca2f22e6f3b16ffa61040c7bbf6fa4fb50
Smig <ahmedharoun370@gmail.com>
2024-01-30 11:12:54 -0500

    Setup ProfileScreen() Composable

---
+++ app/src/main/java/com/example/nyccommunityfridge/ui/profilescreen/ProfileScreen.kt
@@ -0,0 +1,32 @@
+package com.example.nyccommunityfridge.ui.profilescreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nyccommunityfridge.ui.theme.NYCCommunityFridgeTheme
+
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier
+){
+
+    Column {
+
+        // TODO: Exit button
+
+        // TODO : Profile picture
+
+        // TODO: About app button
+
+    }
+
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview(){
+    NYCCommunityFridgeTheme {
+        ProfileScreen()
+    }
+}
