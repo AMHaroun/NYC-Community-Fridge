1ecf2ce7cec5cc574f058a9e0487ea82a37d1a75
Smig <ahmedharoun370@gmail.com>
2024-01-31 10:29:53 -0500

    Created ExitButton() Composable

--- app/src/main/java/com/example/nyccommunityfridge/ui/profilescreen/ProfileScreen.kt
+++ app/src/main/java/com/example/nyccommunityfridge/ui/profilescreen/ProfileScreen.kt
@@ -1,9 +1,14 @@
 package com.example.nyccommunityfridge.ui.profilescreen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.nyccommunityfridge.R
 import com.example.nyccommunityfridge.ui.theme.NYCCommunityFridgeTheme
 
 @Composable
@@ -28,5 +33,26 @@ fun ProfileScreen(
 fun ProfileScreenPreview(){
     NYCCommunityFridgeTheme {
         ProfileScreen()
+    }
+}
+
+@Composable
+fun ExitButton(
+    modifier: Modifier = Modifier,
+    onClick: ()->Unit
+){
+    Icon(
+        painter = painterResource(id = R.drawable.x_icon),
+        contentDescription = stringResource(R.string.exit_button),
+        modifier = modifier.clickable { onClick }
+    )
+
+}
+
+@Preview
+@Composable
+fun ExitButtonPreview(){
+    NYCCommunityFridgeTheme {
+        ExitButton {}
     }
 }
