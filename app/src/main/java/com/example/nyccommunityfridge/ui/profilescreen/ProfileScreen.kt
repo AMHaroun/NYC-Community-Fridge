51d3f7b99d8e075e93fff1de722d953370a0de98
Smig <ahmedharoun370@gmail.com>
2024-02-14 22:00:52 -0500

    Updated ProfileScreen Composable()

--- app/src/main/java/com/example/nyccommunityfridge/ui/profilescreen/ProfileScreen.kt
+++ app/src/main/java/com/example/nyccommunityfridge/ui/profilescreen/ProfileScreen.kt
@@ -1,11 +1,13 @@
 package com.example.nyccommunityfridge.ui.profilescreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +25,7 @@ fun ProfileScreen(
 
     Column(
         modifier = modifier
+            .background(MaterialTheme.colorScheme.primary)
             .fillMaxSize()
             .padding(start = 8.dp) // Adjust the padding as needed
     ) {
@@ -54,6 +57,14 @@ fun ProfileScreen(
                 .align(Alignment.CenterHorizontally),
             textDecoration = TextDecoration.Underline,
             color = Color.Blue,
+        )
+
+        Text(
+            text = "Pictures Posted: "
+        )
+
+        Text(
+            text = "Messages Left: "
         )
 
         Spacer(modifier = Modifier.weight(1f))
